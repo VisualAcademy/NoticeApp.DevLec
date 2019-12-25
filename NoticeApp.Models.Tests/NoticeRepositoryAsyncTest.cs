f2320392b82eecc600bb7650b04aa1eecbf2997e
WebCampKR <webcamp@outlook.kr>
2019-12-25 11:04:30 +0900

    06_GetAll 메서드 코드 구현 및 테스트 코드 생성 후 테스트 완료

--- NoticeApp.Models.Tests/NoticeRepositoryAsyncTest.cs
+++ NoticeApp.Models.Tests/NoticeRepositoryAsyncTest.cs
@@ -14,6 +14,7 @@ namespace NoticeApp.Models.Tests
         [TestMethod]
         public async Task NoticeRepositoryAsyncAllMethodTest()
         {
+            #region [0] DbContextOptions<T> Object Creation and ILoggerFactory Object Creation
             //[0] DbContextOptions<T> Object Creation and ILoggerFactory Object Creation
             var options = new DbContextOptionsBuilder<NoticeAppDbContext>()
                 .UseInMemoryDatabase(databaseName: $"NoticeApp{Guid.NewGuid()}").Options;
@@ -21,7 +22,9 @@ namespace NoticeApp.Models.Tests
 
             var serviceProvider = new ServiceCollection().AddLogging().BuildServiceProvider();
             var factory = serviceProvider.GetService<ILoggerFactory>();
+            #endregion
 
+            #region [1] AddAsync() Method Test
             //[1] AddAsync() Method Test
             using (var context = new NoticeAppDbContext(options))
             {
@@ -30,15 +33,39 @@ namespace NoticeApp.Models.Tests
                 var model = new Notice { Name = "관리자", Title = "공지사항입니다.", Content = "내용입니다." };
 
                 //[B] Act
-                await repository.AddAsync(model);
+                await repository.AddAsync(model); // Id: 1
             }
             using (var context = new NoticeAppDbContext(options))
             {
                 //[C] Assert
                 Assert.AreEqual(1, await context.Notices.CountAsync());
                 var model = await context.Notices.Where(n => n.Id == 1).SingleOrDefaultAsync();
-                Assert.AreEqual("관리자", model.Name); 
+                Assert.AreEqual("관리자", model.Name);
             }
+            #endregion
+
+            #region [2] GetAllAsync() Method Test
+            //[2] GetAllAsync() Method Test
+            using (var context = new NoticeAppDbContext(options))
+            {
+                // 트랜잭션 관련 코드는 InMemoryDatabase 공급자에서는 지원 X
+                //using (var transaction = context.Database.BeginTransaction()) { transaction.Commit(); }
+                //[A] Arrange
+                var repository = new NoticeRepositoryAsync(context, factory);
+                var model = new Notice { Name = "홍길동", Title = "공지사항입니다.", Content = "내용입니다." };
+
+                //[B] Act
+                await repository.AddAsync(model); // Id: 2
+                await repository.AddAsync(new Notice { Name = "백두산", Title = "공지사항입니다." }); // Id: 3
+            }
+            using (var context = new NoticeAppDbContext(options))
+            {
+                //[C] Assert
+                var repository = new NoticeRepositoryAsync(context, factory);
+                var models = await repository.GetAllAsync();
+                Assert.AreEqual(3, models.Count()); // TotalRecords: 3
+            }
+            #endregion
         }
     }
 }
