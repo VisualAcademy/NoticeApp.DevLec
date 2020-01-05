587f4f81e7f4d7b242ef8ef97919e655f03a9427
WebCampKR <webcamp@outlook.kr>
2020-01-05 10:40:29 +0900

    Blazor 프로젝트인 NoticeApp 프로젝트 생성 및 리스트 페이지 구현

---
+++ NoticeApp/Pages/Notices/Index.razor.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Components;
+using NoticeApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoticeApp.Pages.Notices
+{
+    public partial class Index
+    {
+        [Inject]
+        public INoticeRepositoryAsync NoticeRepositoryAsyncReference { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManagerReference { get; set; }
+
+        protected List<Notice> models;
+
+        protected DulPager.DulPagerBase pager = new DulPager.DulPagerBase() 
+        { 
+            PageNumber = 1,
+            PageIndex = 0,
+            PageSize = 2,
+            PagerButtonCount = 5
+        };
+
+        protected override async Task OnInitializedAsync()
+        {
+            await DisplayData();
+        }
+
+        private async Task DisplayData()
+        {
+            //await Task.Delay(3000);
+            var resultsSet = await NoticeRepositoryAsyncReference.GetAllAsync(pager.PageIndex, pager.PageSize);
+            pager.RecordCount = resultsSet.TotalRecords;
+            models = resultsSet.Records.ToList();
+        }
+
+        protected void NameClick(int id)
+        {
+            NavigationManagerReference.NavigateTo($"/Notices/Details/{id}");
+        }
+
+        protected async void PageIndexChanged(int pageIndex)
+        {
+            pager.PageIndex = pageIndex;
+            pager.PageNumber = pageIndex + 1;
+
+            await DisplayData();
+
+            StateHasChanged();
+        }
+    }
+}
