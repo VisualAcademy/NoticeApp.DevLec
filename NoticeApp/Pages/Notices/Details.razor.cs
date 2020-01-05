de0e4cacda003e5e0280fa4c0e2d0d23ee5c6aa2
WebCampKR <webcamp@outlook.kr>
2020-01-05 11:52:52 +0900

    Notices_CRUD 기능 구현 완료

--- NoticeApp/Pages/Notices/Details.razor.cs
+++ NoticeApp/Pages/Notices/Details.razor.cs
@@ -1,15 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Components;
+using NoticeApp.Models;
 using System.Threading.Tasks;
 
 namespace NoticeApp.Pages.Notices
 {
     public partial class Details
     {
-        protected override void OnInitialized()
-        {
+        [Parameter]
+        public int Id { get; set; }
 
+        [Inject]
+        public INoticeRepositoryAsync NoticeRepositoryAsyncReference { get; set; }
+
+        protected Notice model = new Notice();
+
+        protected string content = "";
+
+        protected override async Task OnInitializedAsync()
+        {
+            model = await NoticeRepositoryAsyncReference.GetByIdAsync(Id);
+            content = Dul.HtmlUtility.EncodeWithTabAndSpace(model.Content);
         }
     }
 }
