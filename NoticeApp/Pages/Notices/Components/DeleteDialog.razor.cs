02948cf5107a8632ee07cafa9841fc6a81280a10
RedPlus <redplus@redplus.net>
2020-04-13 04:20:54 +0900

    코드 정리

--- NoticeApp/Pages/Notices/Components/DeleteDialog.razor.cs
+++ NoticeApp/Pages/Notices/Components/DeleteDialog.razor.cs
@@ -5,11 +5,22 @@ namespace NoticeApp.Pages.Notices.Components
 {
     public partial class DeleteDialog
     {
+        #region Parameters
+        /// <summary>
+        /// 부모에서 OnClick 속성에 지정한 이벤트 처리기 실행
+        /// </summary>
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClick { get; set; }
+        #endregion
+
+        #region Properties
         /// <summary>
         /// 모달 다이얼로그를 표시할건지 여부 
         /// </summary>
         public bool IsShow { get; set; } = false;
+        #endregion
 
+        #region Public Methods
         /// <summary>
         /// 폼 보이기 
         /// </summary>
@@ -22,11 +33,6 @@ namespace NoticeApp.Pages.Notices.Components
         {
             IsShow = false;
         }
-
-        /// <summary>
-        /// 부모에서 OnClick 속성에 지정한 이벤트 처리기 실행
-        /// </summary>
-        [Parameter]
-        public EventCallback<MouseEventArgs> OnClick { get; set; }
+        #endregion
     }
 }
